cd152d122ad78a7f351f543db40a12b934f91b04
Shane <>
2021-06-09 23:17:28 -0400

    uses Abstract User in app 'users' for auth

--- Pomodoro_Timer/settings.py
+++ Pomodoro_Timer/settings.py
@@ -126,3 +126,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# to use the model in app "users" instead of the default django one
+AUTH_USER_MODEL = 'users.User'

98ce720c1bd9a3393889e9478ee9476304e54e58
Shane <>
2021-06-09 23:15:12 -0400

    created the app 'users'

--- Pomodoro_Timer/settings.py
+++ Pomodoro_Timer/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'main.apps.MainConfig'
+    'main.apps.MainConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
